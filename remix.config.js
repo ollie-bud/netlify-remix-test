0c03c238287e777521f9485c44b92ccf6d71c2c7
Ollie W <ollie.williams@thisisbud.com>
2023-05-26 02:35:23 +0100

    chore: cleaned up edge streaming

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,21 @@
-const { config } = require("@netlify/remix-edge-adapter");
 const baseConfig =
   process.env.NODE_ENV === "production"
-    ? config
-    : { ignoredRouteFiles: ["**/.*"], future: config.future };
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.ts",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ...baseConfig,
-  // This works out of the box with the Netlify adapter, but you can
+  ignoredRouteFiles: ["**/.*"],
+  // See https://remix.run/docs/en/main/file-conventions/route-files-v2
+  future: {
+    v2_routeConvention: true,
+  },
   // add your own custom config here if you want to.
   //
   // See https://remix.run/docs/en/v1/file-conventions/remix-config
