fb6de68f467bb14c0a9f4bab3e7546203496158b
Ollie W <ollie.williams@thisisbud.com>
2023-05-26 13:58:17 +0100

    chore: clean template slate

---
+++ app/routes/_index.tsx
@@ -0,0 +1,72 @@
+import { useLoaderData, Await, Link } from "@remix-run/react";
+import { Suspense } from "react";
+import { json } from "react-router";
+
+export function headers({
+  loaderHeaders,
+  parentHeaders,
+}: {
+  loaderHeaders: Headers;
+  parentHeaders: Headers;
+}) {
+  console.log(
+    "This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
+  );
+  return {
+    // This is an example of how to set caching headers for a route
+    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
+    "Cache-Control": "public, max-age=60, s-maxage=60",
+  };
+}
+
+export const loader = async () => {
+  const ditto = fetch("https://pokeapi.co/api/v2/pokemon/ditto").then(
+    async (data) => data.json()
+  );
+  const charmander = fetch("https://pokeapi.co/api/v2/pokemon/charmander").then(
+    (data) => data.json()
+  );
+  return json({ ditto: await ditto, charmander: await charmander });
+};
+
+export default function Index() {
+  const { ditto, charmander } = useLoaderData<{
+    ditto: any;
+    charmander: any;
+  }>();
+
+  return (
+    <main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>No Defer (with cache)</h1>
+      <Link to={"/defer"}>Defer</Link>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={ditto} errorElement={<p>Error loading</p>}>
+          {(ditto) => (
+            <>
+              <h1>{ditto?.name}</h1>
+              <img
+                src={ditto?.sprites?.front_default}
+                style={{ imageRendering: "pixelated" }}
+              />
+            </>
+          )}
+        </Await>
+      </Suspense>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={charmander} errorElement={<p>Error loading</p>}>
+          {(charmander) => (
+            <>
+              <h1>{charmander?.name}</h1>
+              <img
+                src={charmander?.sprites?.front_default}
+                style={{ imageRendering: "pixelated" }}
+              />
+            </>
+          )}
+        </Await>
+      </Suspense>
+    </main>
+  );
+}
