0a104137b1e00a276307896a1cff8256932e21f7
Ollie W <ollie.williams@thisisbud.com>
2023-05-26 14:18:46 +0100

    chore: removed route cache

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,23 +2,6 @@ import { useLoaderData, Await, Link } from "@remix-run/react";
 import { Suspense } from "react";
 import { json } from "react-router";
 
-export function headers({
-  loaderHeaders,
-  parentHeaders,
-}: {
-  loaderHeaders: Headers;
-  parentHeaders: Headers;
-}) {
-  console.log(
-    "This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
-  );
-  return {
-    // This is an example of how to set caching headers for a route
-    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
-    "Cache-Control": "public, max-age=60, s-maxage=60",
-  };
-}
-
 export const loader = async () => {
   console.log("/ loader");
 
