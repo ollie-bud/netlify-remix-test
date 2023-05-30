60743a5f6f8b1f682a299db05a7640ec194c81f3
Ollie W <ollie.williams@thisisbud.com>
2023-05-30 15:26:25 +0100

    chore: fixed url

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
   ignoredRouteFiles: ["**/.*"],
-  publicPath: "/build/",
-  routes(defineRoutes) {
-    return defineRoutes((route) => {
-      route("/somewhere/cool/*", "catchall.tsx");
-    });
-  },
-  serverBuildPath: "build/index.js",
 };
